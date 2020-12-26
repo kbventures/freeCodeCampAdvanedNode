9e312f771942b742f15daad2c3a2194552c0a151
kbventures <kmb_x@hotmail.com>
2020-12-26 12:05:22 +0100

    Added Passport and Session

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const { request } = require('https');
 
 const app = express();
 
@@ -14,10 +17,21 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave:true,
+  saveUninitialized: true,
+  cookie: {secure: false}
+}));
+app.use(passport.initialize());
+
 app.set('view engine', 'pug');
+app.set('views', './views/pug')
 
 app.route('/').get((req, res) => {
-  res.render(__dirname + '/views/pug/index.pug', {title: 'Hello World', message: 'Please Login'});
+  console.log(req.session)
+  req.session.count ++
+  res.render('index', {title: 'Hello World', message: 'Please Login'});
 });
 
 app.listen(process.env.PORT || 3000, () => {
